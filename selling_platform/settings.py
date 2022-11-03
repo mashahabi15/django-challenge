a453cab3feb6f1e726f0110745d008cd61df91f3
mashahabi15 <ma.shahabi15@gmail.com>
2022-11-03 08:44:52 +0330

    create app users

--- selling_platform/settings.py
+++ selling_platform/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # selling_platform project apps
+    'users',
+
 ]
 
 MIDDLEWARE = [
